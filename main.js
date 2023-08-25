9b71ad59675e845e54d2f51a2e62225bbfe94670
Kekgethilwe Moloi <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-08-26 00:10:37 +0200

    Add consts and let for mostly player selections
    Added consts to save selections and results of plays (future functions?)
    Started adding actions for what would happen if the player selected any
    button

---
+++ main.js
@@ -0,0 +1,11 @@
+//a place to store all the results of the selections
+const computerSelection = document.getElementById("computerSelection");
+const playerSelection = document.getElementById("playerSelection");
+const result = document.getElementById("result");
+
+//what should happen when user clicks button
+//the range of our selections are the buttons
+const possibleSelection = document.querySelectorAll('button');
+
+//for each button the selection should be saved to a playerSlection
+possibleSelection.forEach(possibleSelection => "click", )
